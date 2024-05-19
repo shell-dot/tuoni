380bab966d935cdbd1be62fc5e610fc11f21616e
tavi.poldma <tavi.poldma@gmail.com>
2024-05-20 01:05:04 +0300

    tuoni autocomplete fixes for zsh

--- scripts/tuoni-autocomplete.sh
+++ scripts/tuoni-autocomplete.sh
@@ -19,10 +19,36 @@ _tuoni_commands() {
     fi
 }
 
+# Zsh-specific autocompletion function
+_tuoni_commands_zsh() {
+    local -a commands client_commands server_commands
+    commands=("help" "start" "stop" "restart" "logs" "clean-configuration" "update" "update-silent" "client" "server")
+    client_commands=("start" "stop" "restart" "logs")
+    server_commands=("start" "stop" "restart" "logs")
+
+    _arguments -C \
+        '1: :->command' \
+        '2: :->subcommand' \
+        '*::arg:->args'
+
+    case "$state" in
+        command)
+            _describe -t commands 'tuoni commands' commands
+            ;;
+        subcommand)
+            if [[ ${words[2]} == "client" ]]; then
+                _describe -t client_commands 'tuoni client commands' client_commands
+            elif [[ ${words[2]} == "server" ]]; then
+                _describe -t server_commands 'tuoni server commands' server_commands
+            fi
+            ;;
+    esac
+}
+
 # Register the completion function
 if [ -n "$ZSH_VERSION" ]; then
     # For Zsh: Use compdef
-    compdef _tuoni_commands tuoni
+    compdef _tuoni_commands_zsh tuoni
 elif [ -n "$BASH_VERSION" ]; then
     # For Bash: Use complete
     complete -F _tuoni_commands tuoni

